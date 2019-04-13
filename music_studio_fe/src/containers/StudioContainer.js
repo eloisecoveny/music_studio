850cd70d01f4e36128e7744b3d5a6e32fe3b0501
Robert Templeton <roberttee1993@googlemail.com>
2019-04-13 12:15:04 +0100

    set up initial file structure

---
+++ music_studio_fe/src/containers/StudioContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class StudioContainer extends Component {
+  render(){
+    return (
+      <div>
+        <h1>I am Studio container!</h1>
+      </div>
+    )
+  }
+}
+
+export default StudioContainer;
