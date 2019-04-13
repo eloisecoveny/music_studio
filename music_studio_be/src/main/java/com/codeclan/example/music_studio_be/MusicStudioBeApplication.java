775f0a46b02c5987895f2e725a0105f9235986ed
Robert Templeton <roberttee1993@googlemail.com>
2019-04-13 12:48:10 +0100

    added mp3s and maven intellij back end

---
+++ music_studio_be/src/main/java/com/codeclan/example/music_studio_be/MusicStudioBeApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.music_studio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStudioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStudioBeApplication.class, args);
+	}
+
+}
