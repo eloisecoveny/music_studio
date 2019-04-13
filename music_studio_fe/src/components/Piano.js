f68af6aed956256382693d2a60a6b4209cbc3f24
Robert Templeton <roberttee1993@googlemail.com>
2019-04-13 16:42:16 +0100

    linked up keys to piano keys

--- music_studio_fe/src/components/Piano.js
+++ music_studio_fe/src/components/Piano.js
@@ -4,10 +4,10 @@ import PianoKey from "./PianoKey"
 
 const Piano = (props) => {
 
-  const notes = ["middle_a", "mid_a_sharp", "middle_b", "middle_c", "mid_c_sharp", "middle_d", "mid_d_sharp", "middle_e", "middle_f", "mid_f_sharp", "middle_g", "mid_g_sharp", "high_a", "high_a_sharp", "high_b", "high_c", "high_c_sharp", "high_d", "high_d_sharp", "high_e", "high_f_sharp", "high_f", "high_g_sharp", "high_g"]
+  const notes = [{note: "middle_a", input: "a"}, {note: "mid_a_sharp", input: "1"}, {note: "middle_b", input: "s"}, {note: "middle_c", input: "q"}, {note: "mid_c_sharp", input: "2"}, {note: "middle_d", input: "w"}, {note: "mid_d_sharp", input: "3"}, {note: "middle_e", input: "e"}, {note: "middle_f", input: "r"}, {note: "mid_f_sharp", input: "4"}, {note: "middle_g", input: "t"}, {note: "mid_g_sharp", input: "5"}, {note: "high_a", input: "y"}, {note: "high_a_sharp", input: "6"}, {note: "high_b", input: "u"}, {note: "high_c", input: "i"}, {note: "high_c_sharp", input: "7"}, {note: "high_d", input: "o"}, {note: "high_d_sharp", input: "8"}, {note: "high_e", input: "p"}, {note: "high_f_sharp", input: "9"}, {note: "high_f", input: "k"}, {note: "high_g_sharp", input: "0"}, {note: "high_g", input: "l"}]
 
   const keys = notes.map((end_link, index) => {
-     return <PianoKey className={end_link} key={ index } sound={require( `../sounds/${props.sound}/${end_link}.mp3` )}></PianoKey>
+     return <PianoKey className={end_link.note} dataKey={ end_link.input } key={ index } sound={require( `../sounds/${props.sound}/${end_link.note}.mp3` )}></PianoKey>
   })
 
   return (
--- music_studio_fe/src/components/PianoKey.js
+++ music_studio_fe/src/components/PianoKey.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "./PianoKey.css"
 
 const PianoKey = (props) => {
+
+  const playKey = () => {
+    const key = document.getElementById(props.className)
+    key.play()
+  }
+
+  function playNote(e){
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    if (audio){
+      audio.play()
+    }
+
+  }
+
+
+  window.addEventListener('keydown', playNote);
+
+
+
   return (
     <div className={props.className}>
-      <div className="key">
-        <audio controls src={props.sound}></audio>
+      <div onClick={playKey} className="key">
+        <audio id={ props.className } data-key={props.dataKey} src={props.sound}></audio>
       </div>
     </div>
   )
